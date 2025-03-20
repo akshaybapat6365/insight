4cfa4a2bb34d62b841c6a44006edba03e3bb50d2
Akshay Bapat <akshay.bapat.31@gmail.com>
2025-03-20 04:51:29 -0600

    Fix: Remove nodeMiddleware flag and runtime directive from middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,8 @@
-// Adding Node.js runtime configuration at the top
-export const runtime = 'nodejs';
-
-import { authMiddleware } from "@clerk/nextjs";
-
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
+import { authMiddleware } from "@clerk/nextjs";
+
 export default authMiddleware({
   // Routes that can be accessed while signed out
   publicRoutes: [
